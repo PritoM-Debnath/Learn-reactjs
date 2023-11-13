569eb69ee2c700f91bee2eb1d5967b5a7d11d2f6
Pritom Debnath <debnathpritom@outlook.com>
2023-11-13 19:32:47 +0600

    Managing state Added

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,26 +1,30 @@
-import {MouseEvent} from "react";
+import {useState} from "react";
 
 function ListGroup() {
   const items = ["New York", "Los Angeles", "Chicago", "Houston"];
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //arr[0] // variable (selectedIndex)
+  //arr[1] // updater function (setSelectedIndex)
 
 
   const getMessage = () => {
     return items.length === 0 && <p>No items</p>;
   };
+
   
-  //event handler
-  const handleClick = (event : MouseEvent) => console.log(event);
   return (
     // this is fragment syntax, which allows us to return multiple elements
     <>
       <h1>List Group</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
             key={item}
-            onClick={handleClick}
+            onClick={()=> setSelectedIndex(index)}
           >
             {item}
           </li>
